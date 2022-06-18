1d9814a9ab253ab0d0ce1329e75e271053c3c01c
LisieAlmeida <lisieuxalmeida@hotmail.com>
2022-06-17 21:47:35 -0300

    Clear App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import { AlbumItem } from './components/albumItem'
 import { Header } from './components/Header'
-import { TypeAlbums } from './types/typeAlbums'
-import { Api } from './api';
-import { useRoutes } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { Page404 } from './pages/Page404'
 import { MainRoutes } from './routes/MainRoutes'
 
 
